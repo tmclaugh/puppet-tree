9983e60f4ae3f5832ec9383a25cb984480aa07b2
Tom McLaughlin <tmclaugh@gmail.com>
2012-06-26 23:32:06 -0300

    Remove old SVN keyword expansion.

--- manifests/exec.pp
+++ manifests/exec.pp
@@ -1,4 +1,3 @@
-# $Id: exec.pp 993 2012-06-21 01:34:45Z tom $
 
 $exec_path = $::operatingsystem ? {
 	centos => ["/bin", "/sbin", "/usr/bin", "/usr/sbin", "/usr/local/bin", "/usr/local/sbin", "/usr/kerberos/sbin", "/usr/kerberos/bin", "/usr/X11R6/bin", "/usr/X11R6/sbin"],
